41f0edaf890c283e0ccd8ee98ecbf9d34e4b4626
pierre1326 <pierre1326@hotmail.com>
2019-04-23 13:02:44 -0600

    Eliminacion de Prints
    Se eliminan los prints y las funciones de prueba de la actividad

--- app/src/main/java/com/example/restaurantes/FiltersActivity.java
+++ app/src/main/java/com/example/restaurantes/FiltersActivity.java
@@ -1,14 +1,12 @@
 package com.example.restaurantes;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
-import android.view.ViewDebug;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -162,8 +160,6 @@ public class FiltersActivity extends AppCompatActivity {
         }
       }
     });
-
-    getFilters(0, 0, this);
 
   }
 
